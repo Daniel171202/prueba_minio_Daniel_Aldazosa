98a669be78c932d2c4b8d11adf698cee9f20872c
Daniel171202 <daniel.aldazosa@gmail.com>
2023-11-23 19:54:44 -0400

    get the file minio link, and put it in the response

--- src/main/java/com/prueba/minio/daniel/aldazosa/prueba_minio/Api/FileController.java
+++ src/main/java/com/prueba/minio/daniel/aldazosa/prueba_minio/Api/FileController.java
@@ -1,11 +1,20 @@
 package com.prueba.minio.daniel.aldazosa.prueba_minio.Api;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.text.html.FormSubmitEvent.MethodType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import io.minio.GetObjectArgs;
+import io.minio.GetObjectResponse;
+import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 
@@ -26,7 +35,25 @@ public class FileController {
                             .stream(file.getInputStream(), file.getSize(), -1)
                             .build()
             );
-            return "File uploaded successfully!";
+
+
+            // ...
+
+            String response = minioClient.getPresignedObjectUrl(
+                GetPresignedObjectUrlArgs.builder()
+                    .method(io.minio.http.Method.GET)
+                    .bucket("mybucket")
+                    .object(file.getOriginalFilename())
+                    .build()
+            );
+
+            System.out.println(response);
+            // Construir la respuesta con el enlace
+            Map<String, String> responseMap = new HashMap<>();
+            responseMap.put("message", "File uploaded successfully!");
+            responseMap.put("download_link", response);
+
+            return responseMap.toString();
         } catch (Exception e) {
             return "Error uploading file: " + e.getMessage();
         }
